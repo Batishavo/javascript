013605cbd5036495c8d09e6315adb1c394637892
Batishavo <romero.martinez.carlosraul@gmail.com>
2021-09-01 17:43:28 -0500

    xd

---
+++ js/bom.js
@@ -0,0 +1,10 @@
+//Ventana del navegador
+console.log("Alto="+window.innerHeight);
+console.log("Ancho="+window.innerWidth);
+//Pantalla
+console.log("Alto="+screen.height);
+console.log("Ancho="+screen.width);
+//url actualiza el
+console.log(window.location);
+window.location.href ="https://www.youtube.com/";
+console.log(window.location);
