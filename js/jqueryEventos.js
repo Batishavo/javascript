7cd985158e7dc3cd549dd2990902918ccc243992
Batishavo <romero.martinez.carlosraul@gmail.com>
2021-09-02 17:45:44 -0500

    video 24

---
+++ js/jqueryEventos.js
@@ -0,0 +1,22 @@
+$("p").click(function(){
+    $("p").html("Texto desde codigo");
+});
+$("#contenido").mouseenter(function () { 
+    $("p").css({'background-color':'pink','font-size':'1.3em'});
+});
+$("#contenido").mouseleave(function (){
+    $("p").css({'background-color':'blue','font-size':'2em','color':'white'});
+});
+//al pulsar teclas
+$("#nombre").keydown(function(){
+    $("#mensaje").html($("#nombre").val());
+});
+//al soltar las teclas
+$("#nombre").keyup(function () { 
+    console.log("soltando tecla :"+$("#nombre").val());
+});
+//agregar varios Eventos
+$("h1").on("mouseleave click",function(){
+    alert("Aplicando dos eventos al titulo");
+});
+$("h1").off("click");
