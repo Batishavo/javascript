8cc7143d16ad77d2bdb1d34ac1732f3e35cc7983
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-10 12:50:34 -0600

    seccion 28

--- javascript/udemyHolamundo/mi-app/src/App.js
+++ javascript/udemyHolamundo/mi-app/src/App.js
@@ -1,9 +1,31 @@
 import Button from "./Button"
 
+
+// class App extends Component {
+//   render(){
+//     return (
+//       <p>
+//         Hola mundo
+//       </p>
+//     )
+//   }
+// }
+
+const arr= [
+  'chanchito feliz',
+  'chanchito triste',
+  'chanchito xd',
+]
+
 const App = () =>{
+  const miVariable = false
+  if(miVariable){
+    return <p>MI variable dio true!</p>
+  }
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1 onClick={(e)=>console.log('clic',e)}>Hola mundo</h1>
+      {arr.map(el => <p key={el}>{el}</p>)}
       <Button onClick={() =>console.log('clickeado')}>
         Enviar
       </Button>
