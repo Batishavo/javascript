bd9a9771ceb0b29ca8f29784096873c44b20e404
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-10 14:02:50 -0600

    video 180

--- javascript/udemyHolamundo/mi-app/src/App.js
+++ javascript/udemyHolamundo/mi-app/src/App.js
@@ -1,36 +1,58 @@
-import Button from "./Button"
+//import Button from "./Button"
+import {Component} from "react"
 
-
-// class App extends Component {
-//   render(){
-//     return (
-//       <p>
-//         Hola mundo
-//       </p>
-//     )
-//   }
-// }
-
-const arr= [
-  'chanchito feliz',
-  'chanchito triste',
-  'chanchito xd',
-]
-
-const App = () =>{
-  const miVariable = false
-  if(miVariable){
-    return <p>MI variable dio true!</p>
+class Button extends Component {
+  render(){
+    console.log('ejecutando metodo render de button');
+    return (
+      <button>
+        Enviar en app
+      </button>
+    )
   }
-  return (
-    <div>
-      <h1 onClick={(e)=>console.log('clic',e)}>Hola mundo</h1>
-      {arr.map(el => <p key={el}>{el}</p>)}
-      <Button onClick={() =>console.log('clickeado')}>
-        Enviar
-      </Button>
-    </div>
-  )
 }
+
+class App extends Component {
+  state= {
+    valor:3
+  }
+  render(){
+    console.log(this.state)
+    return (
+      <div>
+        <p>Hola mundo</p>
+        <Button/>
+        <button 
+          className={`${this.state.valor}`}
+          onClick={()=>this.setState({valor:2})}
+          >
+          Enviar
+        </button>
+      </div>
+    )
+  }
+}
+
+// const arr= [
+//   'chanchito feliz',
+//   'chanchito triste',
+//   'chanchito xd',
+// ]
+
+// const App = () =>{
+//   const miVariable = false
+//   if(miVariable){
+//     return <p>MI variable dio true!</p>
+//   }
+//   return (
+//     <div>
+//       <h1 onClick={(e)=>console.log('clic',e)}>Hola mundo</h1>
+//       {arr.map(el => <p key={el}>{el}</p>)}
+//       <Button onClick={() =>console.log('clickeado')}>
+//         Enviar
+//       </Button>
+//     </div>
+//   )
+// }
 
 export default App
