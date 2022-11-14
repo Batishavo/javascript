0c790ff2295966c3d70d795499cffc70fcfd40c7
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-13 18:40:08 -0600

    fin del curos

--- javascript/udemyHolamundo/carro/src/App.js
+++ javascript/udemyHolamundo/carro/src/App.js
@@ -11,18 +11,51 @@ class App extends Component {
       {name: "ninteno playstation", price: 1500, img: "/assets/nintendo.jpg"},
       {name: "ps5", price: 1500, img: "/assets/ps5.png"},
     ],
-    carro:[
-      
-    ],
+    carro: [],
+    esCarroVisible:false,
   };
+
+  agregarAlCarro= (producto)=>{
+    // console.log(producto)
+    const {carro}=this.state
+    if(carro.find(x=>x.name === producto.name)){
+      const newCarro = carro.map(x=>x.name === producto.name
+        ?({
+          ...x,
+          cantidad:x.cantidad+1
+        })
+        :x
+      )
+      return this.setState({carro:newCarro});
+    }
+    return this.setState({
+      carro:this.state.carro.concat({
+        ...producto,
+        cantidad:1
+      })
+    })
+  }
+  mostrarCarro = () =>{
+    if(this.state.carro.length){
+      this.setState({esCarroVisible:!this.state.esCarroVisible})
+    }
+    this.setState({esCarroVisible:!this.state.esCarroVisible})
+    
+  }
   render() {
+    const {esCarroVisible} = this.state
+    // console.log(this.state.carro)
     return (
       <div>
-        <Navbar/>
+        <Navbar 
+          carro={this.state.carro} 
+          esCarroVisible={esCarroVisible}
+          mostrarCarro={this.mostrarCarro}
+        />
         <Layout>
           <Title />
           <Productos
-            agregarAlCarro={() => console.log("No hace nada")}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
