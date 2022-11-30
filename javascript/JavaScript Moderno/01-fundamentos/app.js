f0d9825d6e262d1f863985c1274b34e863ccd475
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-30 07:43:22 -0600

    video 11

--- javascript/JavaScript Moderno/01-fundamentos/app.js
+++ javascript/JavaScript Moderno/01-fundamentos/app.js
@@ -2,8 +2,15 @@
 
 let a = 10,
   b = 10,
-  c = 10,
-  d = 10,
+  c = "hola ",
+  d = "spiderman",
   x = a + b;
 
-console.log(x);
+const saludo = c + d; 
+
+  // console.log('%c Mis variables','color : blue; font-weight : bold');
+// console.log({a});
+// console.log({b});
+// console.log({c});
+
+// console.table({a, b, x, d, x});
