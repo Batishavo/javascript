8608c46975b303a59cf4d52888edfa49d4c92b6a
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-11-30 20:26:49 -0600

    sección 22

---
+++ javascript/JavaScript Moderno/01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,7 @@
+// alert('Hola mundo')
+
+// let nombre = prompt('Cual es tu nombre', 'sin nombre');
+// console.log(nombrenu);
+
+// const seleccion = confirm('Esta seguro de borrar esto??');
+console.log( global );
