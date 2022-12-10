fec1f637653348aabf63d5ba1251b33e37e0536c
Batishavo <romero.martinez.carlosraul@gmail.com>
2022-12-10 00:43:48 -0600

    video 50

--- javascript/JavaScript Moderno/01-fundamentos/assets/js/bases/operador-ternario.js
+++ javascript/JavaScript Moderno/01-fundamentos/assets/js/bases/operador-ternario.js
@@ -1,5 +1,5 @@
 /**
- * Días de semana abrimos a las 11, 
+ * Días de semana abrimos a las 11,
  * pero los fines de semana abrimos a las 9
  */
 
@@ -7,20 +7,18 @@
 const dia = 1; // 0: domingo... 1:Lunes....
 const horaActual = 10;
 
-
 let horaApertura;
 let mensaje; // Está abierto, Está cerrado, hoy abrimos a las XX
 
 // if ( dia === 0 || dia === 6 ) {
-// if ( [0,6].includes( dia ) ) { 
+// if ( [0,6].includes( dia ) ) {
 //     console.log('Fin de semana');
 //     horaApertura = 9;
 // } else {
 //     console.log('Día de semana');
 //     horaApertura = 11;
 // }
-horaApertura = ( [0,6].includes( dia ) ) ? 9 : 11;
-
+horaApertura = [0, 6].includes(dia) ? 9 : 11;
 
 // if ( horaActual >= horaApertura ) {
 //     mensaje = 'Está abierto';
@@ -28,7 +26,9 @@ horaApertura = ( [0,6].includes( dia ) ) ? 9 : 11;
 //     mensaje = `Está cerrado, hoy abrimos a las ${ horaApertura }`;
 // }
 
-mensaje = ( horaActual >= horaApertura ) ? 'Está abierto' : `Está cerrado, hoy abrimos a las ${ horaApertura }`;
+mensaje =
+  horaActual >= horaApertura
+    ? "Está abierto"
+    : `Está cerrado, hoy abrimos a las ${horaApertura}`;
 
-
-console.log({ horaApertura, mensaje });
+console.log({horaApertura, mensaje});
--- javascript/JavaScript Moderno/02-blacjack/assets/js/juego.js
+++ javascript/JavaScript Moderno/02-blacjack/assets/js/juego.js
@@ -9,18 +9,50 @@ let deck = [];
 const tipos = ["C", "D", "H", "S"],
   especiales = ["A", "J", "Q", "K"];
 
+//Esta duncion cre un nuevo deck
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tipos) {
       deck.push(i + tipo);
     }
   }
-  for(let tipo of tipos){
-    for(let especial of especiales){
-        deck.push(especial + tipo);
+  for (let tipo of tipos) {
+    for (let especial of especiales) {
+      deck.push(especial + tipo);
     }
   }
+  deck = _.shuffle(deck);
   console.log(deck);
+  return deck;
 };
 
 crearDeck();
+
+//Esta funcion me permite tomar una carta
+// console.log(deck.length);
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw "No hay cartas el el deck";
+  }
+  return deck.pop();
+};
+
+//Pedir carts
+
+const valorCarta = carta => {
+  const valor = carta.substring(0, carta.length - 1);
+  // console.log({valor});
+  // 2 = 2 10=1 , 3=3
+  // let puntos = 0;
+  // if (isNaN(valor)) {
+  //   // console.log('No es número');
+  //   puntos = valor === "A" ? 11 : 10;
+  // } else {
+  //   puntos = valor * 1;
+  //   // console.log('Es un número')
+  // }
+  // console.log(puntos);
+  return !isNaN(valor) ? valor * 1 : valor === "A" ? 10 : 11;
+};
+console.log(valorCarta("AD"));
+
