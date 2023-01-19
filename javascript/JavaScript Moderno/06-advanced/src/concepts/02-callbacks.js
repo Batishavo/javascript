984549e59de058cc150b0469ecc7e10067f3cb70
Batishavo <romero.martinez.carlosraul@gmail.com>
2023-01-19 08:32:27 -0600

    video 131

--- javascript/JavaScript Moderno/06-advanced/src/concepts/02-callbacks.js
+++ javascript/JavaScript Moderno/06-advanced/src/concepts/02-callbacks.js
@@ -1,23 +1,39 @@
-import { heroes } from "../data/heroes"
+import {heroes} from "../data/heroes";
 /**
-* 
-* @param {HTMLDivElementa} element
-*/
-export const callbacksComponent = (element)=>{
-    const id = '5d86371f25a058e5b1c8a65e';
-    findHero(id,(hero)=>{
-        element.innerHTML = hero.name;
-    });
-}
-
-/**
- * 
- * @param {String} id 
- * @param {(heroe: Object)=> void} callback 
+ *
+ * @param {HTMLDivElementa} element
  */
-const findHero = (id,callback)=>{
-    
-    const hero = heroes.find(heroe => heroe.id === id);
+export const callbacksComponent = element => {
+  const id1 = "5d86371f25a058e5b1c8a65e";
+  const id2 = "5d86371f1efebc31def272e2";
+  findHero(id1, (error, hero1) => {
+    if (error) {
+      element.innerHTML = error;
+      return;
+    }
 
-    callback(hero);
-}
+    findHero(id2, (error, hero2) => {
+      if (error) {
+        element.innerHTML = error;
+        return;
+      }
+      element.innerHTML = `${hero1.name} / ${hero2.name}`;
+    });
+  });
+};
+
+/**
+ *
+ * @param {String} id
+ * @param {(error: String | Null ,heroe: Object)=> void} callback
+ */
+const findHero = (id, callback) => {
+  const hero = heroes.find(heroe => heroe.id === id);
+
+  if (!hero) {
+    callback(`Hero with id ${id} not found.`);
+    return; //undefined;
+  }
+
+  callback(null, hero);
+};
